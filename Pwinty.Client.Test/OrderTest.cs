f980c548d302e28ce3e950a9a56ff6262ab62fd6
Tom Gallard <tom.gallard@cantab.net>
2013-08-06 06:39:37 +0100

    start on v2

--- Pwinty.Client.Test/OrderTest.cs
+++ Pwinty.Client.Test/OrderTest.cs
@@ -18,5 +18,25 @@ namespace Pwinty.Client.Test
             });
             Assert.IsTrue(result.id > 0);
         }
+        [TestMethod]
+        public void Create_Order_Without_Country_Throws_Exception()
+        {
+            try
+            {
+                PwintyApi api = new PwintyApi();
+                var result = api.Order.Create(new CreateOrderRequest()
+                {
+
+                    payment = Payment.InvoiceRecipient,
+                    qualityLevel = QualityLevel.PRO
+                });
+                Assert.Fail("Exception should be thrown when order created without country code");
+            }
+            catch (PwintyApiException exc)
+            {
+                Assert.IsNotNull(exc.Message);
+            }
+
+        }
     }
 }

7feb7a1c6d2dbb87eacfaaac1786dfa03f9272fa
Tom Gallard <tom.gallard@cantab.net>
2013-08-20 15:00:11 +0100

    monitoring callbacks

---
+++ CFS.PhotoPrintApi.WebApiCallbackTest/CallbackTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pwinty.Client;
+
+namespace CFS.PhotoPrintApi.WebApiCallbackTest
+{
+    [TestClass]
+    public class CallbackTest :TestBase
+    {
+        [TestMethod]
+        public void Receives_Callback_On_Create_Order()
+        {
+            PwintyApi api = new PwintyApi();
+           
+            var result = api.Order.Create(new CreateOrderRequest()
+            {
+                countryCode = "GB",
+                payment = Payment.InvoiceRecipient,
+                qualityLevel = QualityLevel.PRO
+            });
+           
+            Assert.IsTrue(result.id > 0);
+            var tcs = CallbackController.RegisterTaskCompletionSource((int)result.id);
+            var callbackResult = tcs.Task.GetAwaiter().GetResult();
+            Assert.AreEqual(result.id, callbackResult.OrderId);
+            
+        }
+    }
+}
---
+++ CFS.PhotoPrintApi.WebApiCallbackTest/TestBase.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.SelfHost;
+using System.Web.Http;
+using System.Web.Http.Dispatcher;
+
+namespace CFS.PhotoPrintApi.WebApiCallbackTest
+{
+    public class TestBase
+    {
+        static TestBase()
+        {
+            var config = new HttpSelfHostConfiguration("http://localhost:8101")
+                {
+                    
+                };
+                        // Add a route
+            config.Routes.MapHttpRoute(
+              name: "default",
+              routeTemplate: "Callback",
+              defaults: new { controller = "Callback" });
+            System.Web.Http.SelfHost.HttpSelfHostServer server = new System.Web.Http.SelfHost.HttpSelfHostServer(config);
+            server = new HttpSelfHostServer(config);
+            server.OpenAsync();
+        }
+
+
+    }
+}
